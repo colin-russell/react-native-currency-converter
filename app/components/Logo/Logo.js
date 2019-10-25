31f0f1e29011475592fa48a5b7546fc147b3348c
Colin Rusell <colinrussell101@gmail.com>
2019-10-24 23:48:33 -0400

    added Logo component to style to Home screen

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Image, ImageBackground, Text } from 'react-native';
+
+import styles from './styles';
+
+const Logo = () => (
+  <View style={styles.container}>
+    <ImageBackground
+      style={styles.containerImage}
+      resizeMode="contain"
+      source={require('./images/background.png')}
+    >
+      <Image style={styles.image} resizeMode="contain" source={require('./images/logo.png')} />
+    </ImageBackground>
+    <Text style={styles.text}>Currency Converter</Text>
+  </View>
+);
+
+export default Logo;
