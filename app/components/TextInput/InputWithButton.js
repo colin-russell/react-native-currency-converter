205037aa2c1591581516c9cfbf5834703e762d76
Colin Rusell <colinrussell101@gmail.com>
2019-10-25 21:12:57 -0400

    added TextInput InputWithButton component

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, TouchableHighlight, TextInput } from 'react-native';
+import color from 'color';
+
+import styles from './styles';
+
+const InputWithButton = (props) => {
+  const { buttonText, onPress, editable = true } = props;
+
+  const containerStyles = [styles.container];
+  if (editable === false) {
+    containerStyles.push(styles.containerDisabled);
+  }
+
+  const underlayColor = color(styles.$buttonBackgroundColorBase).darken(
+    styles.$buttonBackgroundColorModifier,
+  );
+
+  return (
+    <View style={containerStyles}>
+      <TouchableHighlight
+        underlayColor={underlayColor}
+        style={styles.buttonContainer}
+        onPress={onPress}
+      >
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableHighlight>
+      <View style={styles.separator} />
+      <TextInput style={styles.input} underlineColorAndroid="transparent" {...props} />
+    </View>
+  );
+};
+
+InputWithButton.propTypes = {
+  buttonText: PropTypes.string,
+  onPress: PropTypes.func,
+  editable: PropTypes.bool,
+};
+
+export default InputWithButton;
