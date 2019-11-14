1cc630cba3c1bdbd0eae4e23ecaa0ea856997543
Colin Rusell <colinrussell101@gmail.com>
2019-11-14 16:20:38 -0500

    added ListItem component with associated components

--- app/index.js
+++ app/index.js
@@ -8,7 +8,8 @@ EStyleSheet.build({
   $white: "#FFF",
   $border: "#E2E2E2",
   $inputText: "#797979",
-  $lightGray: "#F0F0F0"
+  $lightGray: "#F0F0F0",
+  $darkText: "#343434"
   // $outline: 1 // this outlines all components for debugging purposes
 });
 
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,15 +1,33 @@
-import React from "react";
-import { SafeAreaView, Text, FlatList } from "react-native";
+import React, { Component } from "react";
+import { SafeAreaView, Text, FlatList, StyleSheet } from "react-native";
+
+import { ListItem, Separator, styles } from "../components/List";
 import currencies from "../data/currencies";
 
-const CurrencyList = () => (
-  <SafeAreaView>
-    <FlatList
-      data={currencies}
-      renderItem={({ item }) => <Text>{item}</Text>}
-      keyExtractor={item => item}
-    />
-  </SafeAreaView>
-);
+const TEMP_CURRENT_CURRENCY = "CAD";
+
+class CurrencyList extends Component {
+  handlePress = () => {
+    console.log("row press");
+  };
+  render() {
+    return (
+      <SafeAreaView>
+        <FlatList
+          data={currencies}
+          renderItem={({ item }) => (
+            <ListItem
+              text={item}
+              selected={item === TEMP_CURRENT_CURRENCY}
+              onPress={this.handlePress}
+            />
+          )}
+          keyExtractor={item => item}
+          ItemSeparatorComponent={Separator}
+        />
+      </SafeAreaView>
+    );
+  }
+}
 
 export default CurrencyList;
