3167411be9824720121475e421418b699078a42f
Colin Rusell <colinrussell101@gmail.com>
2019-11-18 16:36:03 -0500

    added Themes screen

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 // import Home from "./screens/Home";
-import Options from "./screens/Options";
+import Themes from "./screens/Themes";
 
 EStyleSheet.build({
   $primaryBlue: "#4F6D7A",
+  $primaryOrange: "#D57A66",
+  $primaryGreen: "#00BD9D",
+  $primaryPurple: "#9E768F",
   $white: "#FFF",
   $border: "#E2E2E2",
   $inputText: "#797979",
@@ -13,5 +16,5 @@ EStyleSheet.build({
   // $outline: 1 // this outlines all components for debugging purposes
 });
 
-export default () => <Options />;
+export default () => <Themes />;
 // export default () => <Home />;
