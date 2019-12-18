ce5d3d55c302b6cb45024df617b9db3ff88b0b00
Colin Rusell <colinrussell101@gmail.com>
2019-12-18 13:09:44 -0500

    hooked up Reducers to the CurrencyList screen

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -2,7 +2,9 @@ import {
   CHANGE_CURRENCY_AMOUNT,
   SWAP_CURRENCY,
   changeCurrencyAmount,
-  swapCurrency
+  swapCurrency,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY
 } from "../actions/currencies";
 
 // const initialState = {
@@ -58,6 +60,23 @@ const initialState = {
   }
 };
 
+const setConversions = (state, action) => {
+  let conversion = {
+    isFetching: true,
+    date: "",
+    rates: {}
+  };
+
+  if (state.conversions[action.currency]) {
+    conversion = state.conversions[action.currency];
+  }
+
+  return {
+    ...state.conversions,
+    [action.currency]: conversion
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
@@ -71,16 +90,28 @@ const reducer = (state = initialState, action) => {
         baseCurrency: state.quoteCurrency,
         quoteCurrency: state.baseCurrency
       };
+    case CHANGE_BASE_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: action.currency,
+        conversions: setConversions(state, action)
+      };
+    case CHANGE_QUOTE_CURRENCY:
+      return {
+        ...state,
+        quoteCurrency: action.currency,
+        conversions: setConversions(state, action)
+      };
     default:
       return state;
   }
 };
 
-console.log("intialState", initialState);
-console.log("swapCurrency", reducer(initialState, swapCurrency()));
-console.log(
-  "changeCurrencyAmount",
-  reducer(initialState, changeCurrencyAmount("222"))
-);
+// console.log("intialState", initialState);
+// console.log("swapCurrency", reducer(initialState, swapCurrency()));
+// console.log(
+//   "changeCurrencyAmount",
+//   reducer(initialState, changeCurrencyAmount("222"))
+// );
 
 export default reducer;
